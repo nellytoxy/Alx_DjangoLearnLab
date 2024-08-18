de59feddd3d61c3d74d713fac22f40854329567d
Necodine <nellexighodaro@gmail.com.com>
2024-08-18 20:29:22 +0100

    django practice

---
+++ django-models/LibraryProject/relationship_app/UserProfile.py
@@ -0,0 +1,27 @@
+# relationship_app/models.py
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+]
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -25,4 +25,25 @@ class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name  
+        return self.name
+    
+    # relationship_app/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+# relationship_app/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
