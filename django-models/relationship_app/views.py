cadbf6ad64ef17015a75048526605c151407671e
Necodine <nellexighodaro@gmail.com.com>
2024-08-18 20:37:26 +0100

    django practice

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -49,3 +49,27 @@ def user_logout(request):
     logout(request)
     return redirect('login')  # Redirect to login page after logout
 
+# relationship_app/views.py
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import Book, Library
+
+def role_required(role):
+    def in_roles(user):
+        return user.userprofile.role == role
+    return user_passes_test(in_roles)
+
+@login_required
+@role_required('Admin')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@role_required('Librarian')
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@role_required('Member')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
