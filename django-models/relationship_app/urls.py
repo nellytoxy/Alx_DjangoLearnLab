2212f676602ae13a728543fec7521c0033aaa939
Necodine <nellexighodaro@gmail.com.com>
2024-08-18 18:50:32 +0100

    django practice

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -16,16 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
+from .views import list_books, LibraryDetailView, register, user_login, user_logout
 from django.contrib.auth import views as auth_views
-from django.urls import reverse_lazy
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Ensure you have the admin URL if using Django admin
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    # Add other paths like 'home' if needed
 ]
 
 
