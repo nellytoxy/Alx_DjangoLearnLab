db14eb7b62f63b92a753c6e2ed58721ff12a0a08
Necodine <nellexighodaro@gmail.com.com>
2024-08-18 17:40:59 +0100

    django practice

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -116,8 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
 # settings.py
 
 STATIC_URL = '/static/'
