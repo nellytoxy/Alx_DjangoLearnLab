78a903660214710657474ae8dd0781926dc49e95
Necodine <nellexighodaro@gmail.com.com>
2024-08-18 17:32:38 +0100

    django practice

--- django-models/relationship_app/LibraryDetailView.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic.detail import DetailView
-from .models import Library
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
