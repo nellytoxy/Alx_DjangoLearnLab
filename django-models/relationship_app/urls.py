f014a9251f61f80f167567618b4083e8d16af3a3
Necodine <nellexighodaro@gmail.com.com>
2024-08-19 08:17:26 +0100

    django practice

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
      path('books/add/', views.add_book, name='add_book/'),
     path('books/<int:pk>/edit/', views.edit_book, name='edit_book/'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
