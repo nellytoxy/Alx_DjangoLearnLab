b5752ac96b909d97a4cb3296f69783e68f397f1a
Necodine <nellexighodaro@gmail.com.com>
2024-08-19 08:07:53 +0100

    django practice

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .models import Library
 from .forms import BookForm
 
 from django.shortcuts import render
