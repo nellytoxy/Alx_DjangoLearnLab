89629e05c665bb668874ae01ce20419e39a5f9c3
Necodine <nellexighodaro@gmail.com.com>
2024-09-05 07:23:57 +0100

    advanced api creation

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('your_app.api.urls')),  # Replace 'your_app' with the name of your app
 ]
+
