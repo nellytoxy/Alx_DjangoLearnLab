a6ae1cf9ebab10807741141a3e218d910758e41f
Necodine <nellexighodaro@gmail.com.com>
2024-09-05 03:15:28 +0100

    advanced api creation

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .seriealizers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class BookListView(generics.ListCreateAPIView):
+    """
+    API view to list all books or create a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API view to retrieve, update, or delete a book instance.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 # Create your views here.
