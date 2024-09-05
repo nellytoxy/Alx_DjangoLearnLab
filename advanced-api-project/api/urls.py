c3ffde5ff3c8f0366703aa78c7b44eb91be2b718
Necodine <nellexighodaro@gmail.com.com>
2024-09-05 07:17:13 +0100

    advanced api creation

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,12 +8,11 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 ]
+from django.urls import path
+from .views import BookCreateView, BookUpdateView, BookDeleteView
+
 urlpatterns = [
-    path('book/create/', BookCreateView.as_view(), name='book_add'),
-]
-urlpatterns = [
-    path('book/update/', BookUpdateView.as_view(), name='book_edit'),
-]
-urlpatterns = [
-    path('book/delete/', BookDeleteView.as_view(), name='book_delete'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
