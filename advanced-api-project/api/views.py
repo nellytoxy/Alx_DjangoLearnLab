039c0e2773f93d7c6c506fd55ee3cdd4be6287d5
Necodine <nellexighodaro@gmail.com.com>
2024-09-05 16:04:31 +0100

    advanced api creation

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import OrderingFilter, SearchFilter
 import django_filters
 from django_filters import rest_framework
