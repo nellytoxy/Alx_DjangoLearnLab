a08cf57fa95b38b5e5ab71a5c0069d0aa80277ae
Necodine <nellexighodaro@gmail.com.com>
2024-09-14 14:47:04 +0100

    django_blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -16,3 +16,16 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+        
+
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']  # Fields to be included in the form
+
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Add your comment here...'}),
+        }
