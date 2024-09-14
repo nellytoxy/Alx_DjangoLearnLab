d64250f0b1af59b0950cb0387c934058a4f0859a
Necodine <nellexighodaro@gmail.com.com>
2024-09-14 14:24:59 +0100

    django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,4 +19,13 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    
+]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
+    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_edit'),
+    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
