15e3213f293a2a06006a8ac626a6b2a14c4ff39c
Necodine <nellexighodaro@gmail.com.com>
2024-09-14 18:08:11 +0100

    django_blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,6 +4,37 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from .models import Post
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        
+
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']  # Fields to be included in the form
+
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Add your comment here...'}),
+        }
+# blog/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import Post
 from django import forms
 from .models import Post, Tag
 
