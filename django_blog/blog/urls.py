449f036a5d49d726cd6ef03dcdf714dd53b5430a
Necodine <nellexighodaro@gmail.com.com>
2024-09-14 15:51:47 +0100

    django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from .views import post_detail, CommentCreateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
-    # URL pattern for viewing the details of a specific post
-    path('posts/<int:pk>/', post_detail, name='post_detail'),
-    
+   
     # URL pattern for creating a new comment on a specific post
     path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
     
@@ -13,4 +11,8 @@ urlpatterns = [
     
     # URL pattern for deleting a specific comment
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+    
+     # URL pattern for viewing the details of a specific post
+    path('posts/<int:pk>/', post_detail, name='post_detail'),
+    
 ]
