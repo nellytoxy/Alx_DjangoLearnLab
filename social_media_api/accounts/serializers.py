e92613793fc0eb5aa42ebb8d4d48115b2193eb80
Necodine <nellexighodaro@gmail.com.com>
2024-09-20 00:06:06 +0100

    django_social app

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,22 +7,24 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'bio', 'profile_picture')  # Adjust fields as needed
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture')  # Customize as needed
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
+        fields = ('username', 'email', 'password', 'bio', 'profile_picture')  # Include fields as necessary
 
     def create(self, validated_data):
+        # Create a user using the create_user method
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password'],
+            password=validated_data['password'],  # Password should be hashed
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        Token.objects.create(user=user)  # Create token for the user
+        # Create a token for the newly created user
+        Token.objects.create(user=user)
         return user
