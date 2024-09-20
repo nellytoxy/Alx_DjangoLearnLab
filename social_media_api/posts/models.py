a6beab3a4a6ac91e4ed782718bc6af71cb20dea4
Necodine <nellexighodaro@gmail.com.com>
2024-09-20 06:02:02 +0100

    django_social app

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -18,5 +18,14 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Ensure a user can like a post only once
+
+
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
