b35e338d3f3109e74938ba2003c97628191dd1bf
Necodine <nellexighodaro@gmail.com.com>
2024-09-20 07:46:58 +0100

    django_social app

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import get_object_or_404
 from .models import Post, Like
 from notifications.models import Notification
 from django.contrib.contenttypes.models import ContentType  # Import ContentType for notifications
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
 
 from rest_framework import viewsets
 from .models import Post, Comment
@@ -45,3 +49,14 @@ class UnlikePostView(generics.GenericAPIView):
         post = generics.get_object_or_404(Post, pk=pk)  # Retrieve the post or return 404
         Like.objects.filter(user=request.user, post=post).delete()
         return Response({"message": "Post unliked."}, status=204)
+
+
+
+class FollowingPostsViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()  # Assuming 'following' is a ManyToManyField in the User model
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')  # Adjust order as needed
